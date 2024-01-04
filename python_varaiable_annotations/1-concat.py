d2dfae0991ab578f762cbf5879e0896fadf560b3
SMCastr <mariesweetycastro@gmail.com>
2024-01-04 11:09:08 -0400

    str creta file

--- python_varaiable_annotations/1-concat.py
+++ python_varaiable_annotations/1-concat.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+Module with a type-annotated function concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that takes two strings as arguments and returns their concatenation.
+
+    Args:
+        str1 (str): first string.
+        str2 (str): second string.
+
+    Returns:
+        str: The concatenation of the two strings.
+    """
+    return str1 + str2
