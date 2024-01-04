569fa3aad841fdfe595aa7cf6a0d4e624a1ea324
SMCastr <mariesweetycastro@gmail.com>
2024-01-04 10:06:15 -0400

    add file & create

---
+++ python_varaiable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module Basic annotations - add """
+
+
+def add(a: float, b: float) -> float:
+    """ 
+    Function that take tow float as
+    arguments and returns their sum as a float.
+    
+    Args:
+        a (float): first number
+        b (float): second number
+        
+    Returns:
+        float: The sum of two floats.
+    """
+    return a + b
