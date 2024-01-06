fd1286df7f6ba3d20269cfef2681b54e7e333c98
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:15:20 -0400

    fix & modify imports & def

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""Async Comprehension Module"""
+
+
+import asyncio
+from typing import List
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Coroatine that asynchronously collect 10 random numbers
+    using an async comprehensions.
+    """
+
+    result = [i async for i in async_generator()]
+    return result
+
+async def main():
+    """Async function to demonstrate the usage of
+    async_comprehension
+    """
+    print(await async_comprehension())
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""Runtime Measurement Module"""
+
+
+import asyncio
+from typing import List
+from time import time
+
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Coroatine that measures the total runtime for executing async_comprehension
+    four times in parallel.
+    """
+    start_time = time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    
+    total_runtime = time() - start_time
+    
+    return total_runtime
+async def main():
+    """Async function to demonstrate the usage of
+    measure_runtime
+    """
+    return await measure_runtime()
+
+if __name__ == "__main__":
+    print(asyncio.run(main()))
