2ca95d801bc1bd4d8d5ec8520d54c37fe7333061
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:28:54 -0400

    fix struct & spaces

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,13 +5,15 @@ Module with a type-annotated function concat
 
 def concat(str1: str, str2: str) -> str:
     """
-    Function that takes two strings as arguments and returns their concatenation.
+    Function that takes two strings as
+    arguments and returns their concatenation.
 
     Args:
         str1 (str): The first string.
         str2 (str): The second string.
 
     Returns:
-        str: The concatenation of the two strings.
+        str: The concatenation of
+        the two strings.
     """
     return str1 + str2
