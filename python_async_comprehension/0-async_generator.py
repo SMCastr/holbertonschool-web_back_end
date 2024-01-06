e16fd4afd82cb749930ab24b08ff2dfa31111e4b
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:13:33 -0400

    add import async & fix cod print _y

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""Async Generator Module"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Async Generator that yields random numbers between 0 and 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+async def print_yielded_values():
+    """Async function to demonstrate the usage of async generator"""
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(print_yielded_values())
