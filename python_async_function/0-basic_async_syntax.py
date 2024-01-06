dac0a4712b0a00ae38a194ac3b983987f207c70d
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:54:44 -0400

    fxing struct blanck space

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,14 +3,16 @@
 Module with an asynchronous coroutine.
 """
 
+
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and
-    max_delay (included) seconds and eventually returns it.
+    Asynchronous coroutine that waits for a
+    random delay between 0 and max_delay (included)
+    seconds and eventually returns it.
 
     Args:
         max_delay (int): The maximum delay (default is 10).
