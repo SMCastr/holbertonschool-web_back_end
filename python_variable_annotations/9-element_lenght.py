5e942da5930f04f47e3215c2c2070d49b6cdacb2
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:52:34 -0400

    fixin blank module

--- python_variable_annotations/9-element_lenght.py
+++ python_variable_annotations/9-element_lenght.py
@@ -3,7 +3,9 @@
 Module with a function element_length annotated with types
 """
 
+
 from typing import Iterable, Sequence, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
@@ -14,7 +16,8 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
         lst (Iterable[Sequence]): An iterable object containing sequences.
 
     Returns:
-        List[Tuple[Sequence, int]]: A list of tuples, each containing an element from 'lst'
+        List[Tuple[Sequence, int]]: A list of tuples,
+        each containing an element from 'lst'
         and its length as an integer.
     """
     return [(i, len(i)) for i in lst]
