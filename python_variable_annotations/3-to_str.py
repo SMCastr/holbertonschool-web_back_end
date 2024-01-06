419a12055d46381dffb7b41a6fe8c17864feace4
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:39:09 -0400

    str modyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,14 +6,13 @@ Module with a type-annotated function to_str
 
 def to_str(n: float) -> str:
     """
-    Function that takes a float as an argument and
-    returns its string representation.
+    Function that takes a float as an argument
+    and returns its string representation.
 
     Args:
         n (float): The input float.
 
     Returns:
-        str: The string representation of
-        the input float.
+        str: The string representation of the float.
     """
     return str(n)
