d022f5e10bae734f13d59659dd48bb472ac5f98c
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:33:01 -0400

    fix spca math

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -7,12 +7,6 @@ import math
 
 def floor(n: float) -> int:
     """
-    Function that takes a float n as an argument and returns the floor of the float.
-
-    Args:
-        n (float): The input float.
-
-    Returns:
-        int: The floor of the input float, as an integer.
+    Function that takes a float n as argument.
     """
     return math.floor(n)
