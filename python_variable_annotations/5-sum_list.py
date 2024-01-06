b6e3037c41e414792cd3d9d822f2eb32a4c08039
SMCastr <mariesweetycastro@gmail.com>
2024-01-05 23:43:13 -0400

    module fixing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,11 +3,14 @@
 Module with a type-annotated function sum_list
 """
 
+
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Function that takes a list of floats as an argument and returns their sum.
+    Function that takes a list of floats as
+    an argument and returns their sum.
 
     Args:
         input_list (List[float]): The input list of floats.
