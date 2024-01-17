ca5e9c8277fcb6801f451a84ec087b6ed3d2dfc4
SMCastr <mariesweetycastro@gmail.com>
2024-01-17 12:01:17 -0400

    add fucnd code

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,6 +1,7 @@
 // Use 'const' to instantiate whenever possible
 export function taskFirst() {
     const task = "I prefer const when I can.";
+    const task2 = "Also const is a block-scoped variable.";
     return task;
 }
 
@@ -11,6 +12,7 @@ export function getLast() {
 export function taskNext() {
     // Use 'let' when needs to be reassigned
     let combination = "But sometimes let";
+    let combination2 = "is okay too.";
     combination += getLast();
 
     return combination;
