a1d799788858fd57bd1f4e53944d9e2b77a07831
SMCastr <mariesweetycastro@gmail.com>
2024-01-17 14:52:04 -0400

    fix spcaes blanck

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,6 @@
 // Use spread syntax to concatenate arrays and string characters
-export default function concatArrays(array1, array2, string) {
+
+export default function concatArrays(array1, array2, string)
+{
     return [...array1, ...array2, ...string];
-  }
+}
