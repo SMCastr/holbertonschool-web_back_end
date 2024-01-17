d09e5c7328adee65437c0d8c857ea472e933917f
SMCastr <mariesweetycastro@gmail.com>
2024-01-17 11:46:36 -0400

    fix export

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,16 @@
 // Use 'const' to instantiate whenever possible
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
+    const task = "I prefer const when I can.";
     return task;
 }
 
 export function getLast() {
-    return ' is okay';
+    return "is okay";
 }
 
 export function taskNext() {
     // Use 'let' when needs to be reassigned
-    let combination = 'But sometimes let';
+    let combination = "But sometimes let";
     combination += getLast();
 
     return combination;
