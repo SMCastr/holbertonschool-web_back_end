582336c865bf927bf79e1f63516cb3166aa005e9
SMCastr <mariesweetycastro@gmail.com>
2024-01-19 21:14:13 -0400

    modify & fix struc nd

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,16 @@
 /*Use ES6 computed property names for the budget object*/
 function getCurrentYear() {
-	const date = new Date();
-	return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-	const budget = {
-  
-	  [`income-${getCurrentYear()}`]: income,
-	  [`gdp-${getCurrentYear()}`]: gdp,
-	  [`capita-${getCurrentYear()}`]: capita,
-	};
-  
-	return budget;
-  }
+    const date = new Date();
+    return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const budget = {
+
+        [`income-${getCurrentYear()}`]: income,
+        [`gdp-${getCurrentYear()}`]: gdp,
+        [`capita-${getCurrentYear()}`]: capita,
+    };
+
+    return budget;
+}
