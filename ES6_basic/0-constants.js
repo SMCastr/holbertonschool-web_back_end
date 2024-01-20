88aef245b7578fb834eb42885af544999c834c65
SMCastr <mariesweetycastro@gmail.com>
2024-01-19 21:13:23 -0400

    fix spaces

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,16 @@
 /*Use const to declare whenever possible*/
 export function taskFirst() {
-	const task = 'I prefer const when i can.';
-	return task;
-  }
-  
-  export function getLast() {
-	return ' is okay';
-  }
-  
-  export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
-  
-	return combination;
-  }
+    const task = 'I prefer const when i can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
+}
