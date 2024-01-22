e5d5cf43d5605ab27c5ef0538fb54deebdacfee8
SMCastr <mariesweetycastro@gmail.com>
2024-01-22 16:52:03 -0400

    create a getApi &  new promis

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,15 @@
+// function getResponseFromAPI
+function getResponseFromAPI() {
+    // Return a new Promise
+    return new Promise((resolve) => {
+      // Resolve the Promise with an object representing a successful API response
+      resolve({
+        status: 200,
+        body: 'photo-profile-1',
+      });
+    });
+  }
+  
+  // Export the function to make it available to other modules
+  export default getResponseFromAPI;
+  
