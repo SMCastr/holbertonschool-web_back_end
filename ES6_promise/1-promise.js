4d8bccc269ab2ab4bf07ba127bfc89bd85c5f979
SMCastr <mariesweetycastro@gmail.com>
2024-01-22 16:53:03 -0400

    o funct new

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+// success and failure
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  
