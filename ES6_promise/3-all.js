00aaf33a44525e64079214306e183b48f3265da0
SMCastr <mariesweetycastro@gmail.com>
2024-01-22 16:55:07 -0400

    import all cath

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+// Run this file with Node (ES6)
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
