2b1ea1b2093631ddf2d8cd62429eb0bfe28fd1d2
SMCastr <mariesweetycastro@gmail.com>
2024-01-24 11:01:50 -0400

    fix resovle

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,20 +1,13 @@
-/*Define the function getFullResponseFromAPI*/
-function getFullResponseFromAPI(success) {
-  // Return a new Promise
+
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    // Check the value of the success parameter
     if (success) {
-      // If true, resolve the Promise with a success response
       resolve({
         status: 200,
-        body: 'Success',
+        body: 'Success'
       });
     } else {
-      // If false, reject the Promise with an error message
       reject(new Error('The fake API is not working currently'));
     }
   });
 }
-
-// Export the function to make it available to other modules
-export default getFullResponseFromAPI;
