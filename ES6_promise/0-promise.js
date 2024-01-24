04972e1eb8fe180f1c0a8d7923c5d6558cf9d16a
SMCastr <mariesweetycastro@gmail.com>
2024-01-24 10:59:57 -0400

    modify code fucn

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,15 +1,6 @@
-/*function getResponseFromAPI*/
-function getResponseFromAPI() {
-    // Return a new Promise
-    return new Promise((resolve) => {
-      // Resolve the Promise with an object representing a successful API response
-      resolve({
-        status: 200,
-        body: 'photo-profile-1',
-      });
-    });
-  }
-  
-  // Export the function to make it available to other modules
-  export default getResponseFromAPI;
-  
+/* This function returns a promise*/
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
+  });
+}
