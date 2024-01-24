7198c50c409909e9bdb725fe522c89ac402e278a
SMCastr <mariesweetycastro@gmail.com>
2024-01-24 13:51:09 -0400

    fix fucnt & promise cons

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,16 @@
-/* this return a promise object */
+// 0-promise.js
+
+
+/**
+ * Function that returns a resolved Promise.
+ * @returns {Promise} - Resolved Promise.
+ */
+
 
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    if (true) {
+    const condition = true;
+    if (condition) {
       resolve();
     } else {
       reject();
--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,10 +1,19 @@
+// 1-promise.js
+
+
+/**
+ * Returns a promise based on the success parameter.
+ * @param {boolean} success - Indicates whether the API call is successful.
+ * @returns {Promise} - A promise that resolves with a success object or rejects with an error.
+ */
+
 
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success'
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
