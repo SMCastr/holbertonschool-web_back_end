847ca03eebcc310ea7f82e0ecaf70ca3f79c7500
SMCastr <mariesweetycastro@gmail.com>
2024-01-24 11:07:22 -0400

    try fix & update

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,21 @@
-// Create and handle it with a then
+/*Define the function handleResponseFromAPI*/
 export default function handleResponseFromAPI(promise) {
-    return promise
-      .then((result) => {
-        console.log('Got a response from the API');
-        return { status: 200, body: 'success' };
-      })
-      .catch(() => new Error());
-  }
-  
+  // Append handlers to the given Promise using the .then() and .catch() methods
+  promise
+    .then((result) => {
+      // When the Promise resolves, log a message to the console
+      console.log('Got a response from the API');
+
+      // Return an object representing a successful response
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      // When the Promise rejects, log a message to the console
+
+      // Return an empty Error object
+      return new Error();
+    });
+}
