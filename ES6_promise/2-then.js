88a5c642b05085ba2618ecd92700071e5b1bfb22
SMCastr <mariesweetycastro@gmail.com>
2024-01-24 14:06:33 -0400

    create respons & fix func api

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,21 +1,20 @@
-/*Define the function handleResponseFromAPI*/
-export default function handleResponseFromAPI(promise) {
-  // Append handlers to the given Promise using the .then() and .catch() methods
-  promise
-    .then((result) => {
-      // When the Promise resolves, log a message to the console
-      console.log('Got a response from the API');
+// 2-then.js
 
-      // Return an object representing a successful response
-      return {
-        status: 200,
-        body: 'success',
-      };
+
+/**
+ * Handles the response from the API promise.
+ * @param {Promise} promise - The API promise.
+ * @returns {Promise} - A new promise with attached handlers.
+ */
+
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
     })
     .catch((error) => {
-      // When the Promise rejects, log a message to the console
-
-      // Return an empty Error object
       return new Error();
     });
 }
--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,27 @@
-// Run this file with Node (ES6)
+// 3-all.js
+
+
 import { uploadPhoto, createUser } from './utils';
 
+
+/**
+ * Handles the profile signup by collectively resolving promises.
+ */
+
+
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  Promise.allSettled([uploadPhoto(), createUser()])
+    .then((results) => {
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          const { body, firstName, lastName } = result.value;
+          console.log(`${body} ${firstName} ${lastName}`);
+        } else {
+          console.log('Signup system offline');
+        }
+      });
     })
-    .catch(() => {
-      console.log('Signup system offline');
+    .catch((error) => {
+      console.error('Error during signup:', error);
     });
 }
