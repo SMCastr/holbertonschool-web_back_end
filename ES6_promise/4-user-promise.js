7e654bb17ca9e5253d675a80f402dc6774185b27
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 15:44:07 -0400

    fix object

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,14 +1,14 @@
 // 4-user-promise.js
 
 /**
- * Retorna una promesa resuelta con un objeto que contiene los campos firstName y lastName.
- * @param {string} firstName - El nombre.
- * @param {string} lastName - El apellido.
- * @returns {Promise} - Una promesa resuelta con un objeto que tiene los campos firstName y lastName.
+ * Returns a resolved promise with an object containing the fields firstName and lastName.
+ * @param {string} firstName - The first name.
+ * @param {string} lastName - The last name.
+ * @returns {Promise} - A resolved promise with an object having.
  */
 
 export default function signUpUser(firstName, lastName) {
-  // Usando la notación de propiedad abreviada para mejorar la legibilidad
+  // Using object shorthand notation to improve readability
   return Promise.resolve({
     firstName,
     lastName,
