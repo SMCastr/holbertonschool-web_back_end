d1c4ef83156e690817e55bad8273030bb799256f
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 20:47:21 -0400

    then

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,18 +1,11 @@
 // 2-then.js
 
-/**
- * Handles the response from the API promise.
- * @param {Promise} promise - The API promise.
- * @returns {Promise} - A new promise with attached handlers.
- */
-
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch((error) => {
-      return new Error();
-    });
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
