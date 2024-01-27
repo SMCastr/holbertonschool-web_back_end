8f53af5e50cadf25088dabfd9fc0c0f70d1448be
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 20:38:07 -0400

    d fix cod

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,26 +1,10 @@
 // 3-all.js
-
-
 import { uploadPhoto, createUser } from './utils';
 
-/**
- * Handles the profile signup by collectively resolving promises.
- */
-
-
 export default function handleProfileSignup() {
-  Promise.allSettled([uploadPhoto(), createUser()])
-    .then((results) => {
-      results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          const { body, firstName, lastName } = result.value;
-          console.log(`${body} ${firstName} ${lastName}`);
-        } else {
-          console.log('Signup system offline');
-        }
-      });
-    })
-    .catch((error) => {
-      console.error('Error during signup:', error);
-    });
+return Promise.all([uploadPhoto(), createUser()])
+  .then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  })
+  .catch(() => console.log('Signup system offline'));
 }
