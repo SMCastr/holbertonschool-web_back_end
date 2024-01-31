8e5af61c2f1572c4700f562963bc53a182169960
SMCastr <mariesweetycastro@gmail.com>
2024-01-31 14:41:03 -0400

    fix spaces struc

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,9 @@
-/* Returns an array of students located in a specific city using the filter function */
+/* Returns an array of students located in a specific
+city using the filter function */
+
+
 const getStudentsByLocation = (students, city) =>
 	students.filter((student) => student.location === city);
 
+	
 export default getStudentsByLocation;
