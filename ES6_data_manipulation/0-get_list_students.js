3ac516be549a0c6f37d149b643408560931c8d0c
SMCastr <mariesweetycastro@gmail.com>
2024-02-01 11:51:30 -0400

    strce spcs &fix

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,19 +2,19 @@
 
 /**
  * Function to create a list of students.
- * @returns {Array} - Array of student objects with id, firstName, and location attributes.
+ * @returns {Array} - Array of student
+ * objects with id, firstName,
+ * and location attributes.
  */
 function getListStudents() {
-	// List of students in the specified order
 	const students = [
-	  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	  { id: 2, firstName: 'James', location: 'Columbia' },
-	  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+		{ id: 2, firstName: 'James', location: 'Columbia' },
+		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
 	];
-  
+
 	return students;
   }
   
-  // Exporting the function to make it accessible from other files
   export default getListStudents;
   
