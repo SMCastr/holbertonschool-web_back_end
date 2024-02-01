3d20b6c2c8c866f8561d41e421d63a2a8e05b434
SMCastr <mariesweetycastro@gmail.com>
2024-02-01 11:46:53 -0400

    fx Updt cod

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,20 @@
-/*Creates an array of student objects with id
-firstName, and location attributes*/
-const getListStudents = () => [
-	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	{ id: 2, firstName: 'James', location: 'Columbia' },
-	{ id: 5, firstName: 'Serena', location: 'San Francisco' },
-];
+// 0-get_list_students.js
 
-
-export default getListStudents;  
+/**
+ * Function to create a list of students.
+ * @returns {Array} - Array of student objects with id, firstName, and location attributes.
+ */
+function getListStudents() {
+	// List of students in the specified order
+	const students = [
+	  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	  { id: 2, firstName: 'James', location: 'Columbia' },
+	  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+	];
+  
+	return students;
+  }
+  
+  // Exporting the function to make it accessible from other files
+  export default getListStudents;
+  
--- ES6_data_manipulation/1-get_list_students_ids.js
+++ ES6_data_manipulation/1-get_list_students_ids.js
@@ -1,9 +1,23 @@
-/* Creates an array of student objects with id, firstName, and location attributes */
-const getListStudents = () => [
-	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	{ id: 2, firstName: 'James', location: 'Columbia' },
-	{ id: 5, firstName: 'Serena', location: 'San Francisco' },
-];
+// 1-get_list_student_ids.js
 
-
-export default getListStudents;
+/**
+ * Function to get an array of student ids from a list of student objects.
+ * @param {Array} studentsList - Array of student objects with id, firstName, and location attributes.
+ * @returns {Array} - Array of student ids.
+ */
+function getListStudentIds(studentsList) {
+	// Check if the input is an array
+	if (!Array.isArray(studentsList)) {
+	  // If not an array, return an empty array
+	  return [];
+	}
+  
+	// Use map to extract ids from the array of student objects
+	const studentIds = studentsList.map((student) => student.id);
+  
+	return studentIds;
+  }
+  
+  // Export the function to make it accessible from other files
+  export default getListStudentIds;
+  
